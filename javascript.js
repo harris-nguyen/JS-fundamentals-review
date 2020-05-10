99ece00f2a4654e730145eccd0e643f84fff546c
harris-nguyen <harrisq.nguyen@gmail.com>
2020-05-10 01:10:42 +0000

    review

--- javascript.js
+++ javascript.js
@@ -1,9 +1,24 @@
 function addFullNameProperty(obj) {
-  return `${obj.firstName} ${obj.lastName}`;
+  return `${obj.firstName} ${obj.lastName}`
 }
 var person = {
-  firstName: "Jade",
-  lastName: "Smith",
+  firstName: 'Jade',
+  lastName: 'Smith'
 };
 
-console.log(addFullNameProperty(person)); // --> 'Jade Smith'
+console.log(addFullNameProperty(person))// --> 'Jade Smith'
+
+function addObjectProperty(obj1, key, obj2) {
+  return obj1[key] = obj2
+}
+
+var person1 = {
+  name: "Joe Blow",
+  role: "schlub",
+};
+var person2 = {
+  name: "Mr. Burns",
+  role: "supervisor",
+};
+
+console.log(addObjectProperty(person1, "manager", person2)); // --> { name: 'Mr. Burns', role: 'supervisor' }
