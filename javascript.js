4631c96cb92159a9173e8c5051fda075b2ab34ef
harris-nguyen <harrisq.nguyen@gmail.com>
2020-05-10 01:20:29 +0000

    review

--- javascript.js
+++ javascript.js
@@ -5,8 +5,8 @@ var person = {
   firstName: 'Jade',
   lastName: 'Smith'
 };
-
 console.log(addFullNameProperty(person))// --> 'Jade Smith'
+//
 
 function addObjectProperty(obj1, key, obj2) {
   return obj1[key] = obj2
@@ -20,5 +20,20 @@ var person2 = {
   name: "Mr. Burns",
   role: "supervisor",
 };
-
 console.log(addObjectProperty(person1, "manager", person2)); // --> { name: 'Mr. Burns', role: 'supervisor' }
+//
+
+function addArrayProperty(obj, key, arr) {
+  return obj[key] = arr
+}
+var myObj = {};
+var myArray = [1, 3];
+addArrayProperty(myObj, 'myProperty', myArray);
+console.log(addArrayProperty(myObj, 'myProperty', myArray)); // --> [1, 3]
+//
+
+function getNthElement(array, n) {
+  return array[n]
+}
+var output = getNthElement([1, 3, 5], 1);
+console.log(output); // --> 3
